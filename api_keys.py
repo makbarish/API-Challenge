19bd521f3163480000a23f39dbfc146011890f1b
makbarish <80001675+makbarish@users.noreply.github.com>
2021-06-02 21:35:48 -0500

    competed weatherPy

--- api_keys.py
+++ api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "c3102f2b1e0b962abd5695d5cb3336cd"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyAVi0_B58a2RSu4FnJfZhwkSLyJYqq0Mgo"
